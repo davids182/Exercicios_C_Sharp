c34ca918a1400ce28e788ca49399e79e29305f28
David Oliveira <davi.exe@hotmail.com>
2021-10-16 10:06:08 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios_C_Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
